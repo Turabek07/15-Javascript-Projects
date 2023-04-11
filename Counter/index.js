92f4d7ee34b80154e7f58d8658d55ee47aaccaf1
Turabek <turabekyusubov587@gmail.com>
2023-04-12 04:16:34 +0500

    completed project with added dom,forEach loop and if else statement i used

---
+++ Counter/index.js
@@ -0,0 +1,29 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+        const styles = event.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "black";
+        }
+        value.textContent = count;
+
+    })
+})
