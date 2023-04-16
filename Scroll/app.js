fa4a9f61097436004f74e866c554fd2ef51d5dee
Turabek <turabekyusubov587@gmail.com>
2023-04-16 20:21:52 +0500

    give links functionality and proper height

--- Scroll/app.js
+++ Scroll/app.js
@@ -37,4 +37,34 @@ window.addEventListener("scroll",function(){
     else{
         topLink.classList.remove("show-link");
     }
-})
+});
+
+
+///select links 
+
+const scrollLinks = document.querySelectorAll(".scroll-link")
+scrollLinks.forEach(function(link){
+    link.addEventListener("click",function(e){
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        // calculate height
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+        if(!fixedNav ){
+            position = position - navHeight 
+        }
+        if(navHeight > 82){
+            position = position + containerHeight
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
