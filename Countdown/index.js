f57a259984cee5dee3b52bb06650b7561e6906f5
Turabek <turabekyusubov587@gmail.com>
2023-04-17 19:31:58 +0500

    added setInterval() function and clearInterval()function

--- Countdown/index.js
+++ Countdown/index.js
@@ -45,6 +45,7 @@ const futureTime = futureDate.getTime()
 
 function getRemainingTime() {
   const today = new Date().getTime();
+  const t = futureTime - today
   const oneDay = 24 * 60 * 60 * 1000;
   const oneHour = 60 * 60 * 1000;
   const oneMinute = 60 * 1000;
@@ -56,5 +57,24 @@ function getRemainingTime() {
 
   //set values array
 
-  const values = [days,hours,minutes,seconds]
+  const values = [days, hours, minutes, seconds];
+
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`)
+    }
+    return item;
+  }
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index])
+  })
+  if(t<0){
+    clearInterval(countdown)
+    deadline.innerHTML = `<h4 class = "expired">sorry , this giveaway has expired</h4> `
+  }
 }
+//countdown
+let countdown = setInterval(getRemainingTime,1000)
+
+getRemainingTime()
