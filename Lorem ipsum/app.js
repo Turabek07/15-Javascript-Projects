061c345aa24c0e8381d4937fd3103d0fdf7d6fe3
Turabek <turabekyusubov587@gmail.com>
2023-04-17 20:18:49 +0500

    completed with added slice(),map()functions

--- Lorem ipsum/app.js
+++ Lorem ipsum/app.js
@@ -17,5 +17,18 @@ const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text")
 
 form.addEventListener("submit",function(e){
-    e.p
-})
+    e.preventDefault();
+    const value = parseInt(amount.value);
+   const random = Math.floor(Math.random() * text.length)
+
+    if(isNaN(value) || value < 0 || value > 9){
+        result.innerHTML = `<p class = "result">${text[random]}</p>`;
+    }
+    else{
+        let tempText = text.slice(0,value);
+       tempText = tempText.map(function(item){
+        return `<p class="result">${item}</p>`
+       }).join();
+       result.innerHTML = tempText;
+    }
+});
