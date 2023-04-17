e41742db740e284537944566d32cdf510881a0a7
Turabek <turabekyusubov587@gmail.com>
2023-04-17 19:41:43 +0500

    completed with added to futuredate variable dynamicly get year month,days,min,secs

--- Countdown/index.js
+++ Countdown/index.js
@@ -25,9 +25,14 @@ const weekdays = [
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".dealine");
 const items = document.querySelectorAll(".deadline-format h4");
-let futureDate = new Date(2023, 6, 2, 11, 30, 0);
-//  console.log(futureDate)
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear()
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate()
 
+// let futureDate = new Date(2023, 6, 2, 11, 30, 0);
+//  console.log(futureDate)
+const futureDate = new Date(tempYear,tempMonth,tempDay +1,18, 30, 0)
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 let mins = futureDate.getMinutes();
@@ -69,12 +74,12 @@ function getRemainingTime() {
   items.forEach(function (item, index) {
     item.innerHTML = format(values[index])
   })
-  if(t<0){
+  if (t < 0) {
     clearInterval(countdown)
     deadline.innerHTML = `<h4 class = "expired">sorry , this giveaway has expired</h4> `
   }
 }
 //countdown
-let countdown = setInterval(getRemainingTime,1000)
+let countdown = setInterval(getRemainingTime, 1000)
 
 getRemainingTime()
