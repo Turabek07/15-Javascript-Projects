28fdf0a197b6944517064eb518d9855655e1b4cd
Turabek <turabekyusubov587@gmail.com>
2023-04-18 07:16:43 +0500

    slect items with querySelector and byId

--- Grocery/app.js
+++ Grocery/app.js
@@ -1,7 +1,16 @@
 //** ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const sumbitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 //* edit option
-
+let editElement;
+let editFlag = false;
+let editId = "";
 // ****** EVENT LISTENERS **********
 
 // ****** FUNCTIONS **********
